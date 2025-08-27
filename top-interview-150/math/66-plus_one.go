1bc39ff8346443ccc61cb2f9a8afa459c945f2c4
Songwaad H <songw44d@gmail.com>
2025-08-27 11:25:24 +0700

    feat: add leetcode 66 - plus one

---
+++ top-interview-150/math/66-plus_one.go
@@ -0,0 +1,20 @@
+// 66. Plus One
+// Link: https://leetcode.com/problems/plus-one
+// Time Complexity: O(n)
+
+package math
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+
+	digits = append([]int{1}, digits...)
+
+	return digits
+}
