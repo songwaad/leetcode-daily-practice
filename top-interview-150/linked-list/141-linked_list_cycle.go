b88d602604f7795f493ca28fe57ec1646b3f86ca
Songwaad H <songw44d@gmail.com>
2025-08-27 10:53:39 +0700

    feat: add leetcode 141 - linked list cycle

---
+++ top-interview-150/linked-list/141-linked_list_cycle.go
@@ -0,0 +1,29 @@
+// 141. Linked List Cycle
+// Link: https://leetcode.com/problems/linked-list-cycle
+// Time Complexity: O(n)
+
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+
+	return false
+}
